11b61967f83a90da1e1ba2d5861cf657c9561b8e
ItsBr0dyy <231164304+ItsBr0dyy@users.noreply.github.com>
2025-11-23 19:34:18 -0800

    Update orbit.py

--- orbit.py
+++ orbit.py
@@ -25,7 +25,7 @@ logo = '''
 while True:
     os.system('cls')
     print(logo)
-    os.system('title Orbit IP by ItsBr0dyyDev')
+    os.system('title Orbit IP - by ItsBr0dyyDev')
     x = input('Press Enter to Start!')
 
     if x == '':
@@ -34,12 +34,17 @@ while True:
         r = requests.get(f'http://ip-api.com/json/{IP}')
         data = r.json()
         print
-        print('IP Results\n')
+        print('Data is processing...')
+        print('Data has been collected...')
+        print('Here is the data: ')
         print('')
+        print('IP Results\n')
+        print(f"The status of tracking this IP was a {data['status']}!")
         print(f"Country: {data['country']}")
         print(f"Region: {data['regionName']}")
         print(f"City: {data['city']}")
         print(f"Zip: {data['zip']}")
+        print(f"Timezone: {data['timezone']}")
         print(f"ISP: {data['isp']}")
         print(f"IP: {data['query']}")
         print('')
